116466340f37d0593f5b0096e003e5e2af4fd132
Zach <zmewshaw@gmail.com>
2021-09-23 14:19:59 -0400

    Updated

--- MasonCC/IDEF0.py
+++ MasonCC/IDEF0.py
@@ -1,11 +1,19 @@
-raw_IV = "942a502bd5ff741f21c734a824ada7772412ee5cfb3ef345"
-raw_C0 = "3a424c529b626e3"
-raw_C1 = "4162c9243a645e2"
-IV = []
-C0 = []
-C1 = []
-for i in range(len(raw_IV)):
-    IV.append(bin(ord(raw_IV[i])))
-for i in range(len(raw_C0)):
-    C0.append(bin(ord(raw_C0[i])))
-    C1.append(bin(ord(raw_C1[i])))
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+I1 = "942a502bd5ff741f21c734a824ada7772412ee5cfb3ef345"
+I2 = "3a424c529b626e3"
+I3 = "4162c9243a645e2"
+
+# translated vars
+key = bytes.fromhex(I1)
+iv = bytes.fromhex(I2)
+ct = bytes.fromhex(I3)
+print(key)
+print(iv)
+print(ct)
+def blowfish_decrypt(key, iv, ct):
+    cipher = Cipher(algorithms.Blowfish(key), modes.CFB(iv))
+    decryptor = cipher.decryptor()
+    print((decryptor.update(ct) + decryptor.finalize()).hex())
+
+blowfish_decrypt(key, iv, ct)
