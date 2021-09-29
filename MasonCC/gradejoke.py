ee8bf0edb910c61ee92e28484b1150509b3df4d8
Zach <zmewshaw@gmail.com>
2021-09-29 13:35:08 -0400

    Updated

--- MasonCC/gradejoke.py
+++ MasonCC/gradejoke.py
@@ -3,12 +3,7 @@ import imageio
 import qrtools
 
 gif = imageio.get_reader('riddle.gif')
-print(gif)
 num_frames = len(gif)
 
 for frame in gif:
-    f1 = Image.fromarray(frame)
-    qr = qrtools.QR()
-    qr.decode(f1)
-    print(qr.data)
     print(frame)
