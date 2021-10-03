602833fe2340e632e2d7b18fbe7b6a913beb1ec2
zmewshaw <60802390+zmewshaw@users.noreply.github.com>
2021-10-02 22:06:37 -0400

    Updated

--- c_practice/switch.c
+++ c_practice/switch.c
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int slices = 3;
+
+    switch(slices)
+    {
+        case 1:
+            printf("you did a great job!");
+            break;
+        case 2:
+            printf("you did an ok job");
+            break;
+        case 3:
+            printf("you did a bad job");
+            break;
+    }
     return 0;
 }
